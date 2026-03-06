0e3b3955d960fdd46d7a9e7d66e288b4fe0137a6
danut462 <danut.pop@uavia.eu>
2026-03-06 11:55:30 +0100

    chore(main): release 2.3.0 (#15)
    * chore(main): release 2.3.0
    
    * chore: update conanfile.py dependencies
    
    * [cascade]
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- conanfile.py
+++ conanfile.py
@@ -92,10 +92,10 @@ class UaviaAutopilotGenericInterface(ConanFile):
             "docopt.cpp/0.6.3",
             "nlohmann_json/3.9.1",
             f"gtest/{self.options.gtest_version}",
-            "uavia-ckt/main@conan/stable",
-            "uavia-navtools/main@conan/stable",
-            "uavia-protocol/main@conan/stable",
-            "uavia-srp/main@conan/stable"
+            "uavia-ckt/2.12.1@conan/stable",
+            "uavia-navtools/1.6.3@conan/stable",
+            "uavia-protocol/2.7.9@conan/stable",
+            "uavia-srp/0.74.0@conan/stable"
         ] # main dependencies
         for autopilot in self.autopilots:
             deps.extend(autopilot.get_build_dependencies()) # extend with autopilot build dependencies
